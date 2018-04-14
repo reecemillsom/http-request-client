2e08cb096760268ab6691a01c6cf7deffca4ba16
Reece <reecemillsom@gmail.com>
2018-04-14 14:11:39 +0100

    exporting post logic

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import {XMLHttpRequest} from "xmlhttprequest";
 import {GetRequest} from "./src/Request/GetRequest/GetRequest";
+import {PostRequest} from "./src/Request/PostRequest/PostRequest";
 import {XMLHttpRequestFactory} from "./src/XMLHttpRequestFactory/XMLHttpRequestFactory";
 
 const factory = new XMLHttpRequestFactory(XMLHttpRequest),
-	Get = new GetRequest(factory);
+	Get = new GetRequest(factory),
+	Post = new PostRequest(factory);
 
 export {
-	Get
+	Get,
+	Post
 };
 
 
